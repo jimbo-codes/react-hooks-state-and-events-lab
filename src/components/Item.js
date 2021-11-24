e0376026c54c2831a955a6f148be47e214d2bca7
James Burns <jrburns96@aol.com>
2021-11-24 18:14:07 -0500

    commit tree 73095f6cd02b7fed911eb540414c4296b8954757

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, addCart] = useState(false);
   return (
-    <li className="">
+    <li className={inCart?"in-cart":""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={()=>addCart(!inCart)}className="add">{inCart?"Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, selectThis] = useState("All");
+console.log(items);
+  const result = items.filter(item=>{
+    if(selectedCategory==="All"){
+      return items;
+    }else{
+      return item.category === selectedCategory
+    }
+        })
+        console.log(result)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(e)=>{
+          selectThis(e.target.value)
+        }}
+      
+        name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* You're going to replace items with the new filtered array result */}
+        {result.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
